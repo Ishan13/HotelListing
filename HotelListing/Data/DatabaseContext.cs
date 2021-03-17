9e1afcc2021d39d1c61fe46c85d8e767ca749126
ishan shrestha <shrestha.ishan2@gmail.com>
2021-03-17 10:33:48 +0545

    today's commit

--- HotelListing/Data/DatabaseContext.cs
+++ HotelListing/Data/DatabaseContext.cs
@@ -9,12 +9,57 @@ namespace HotelListing.Data
     public class DatabaseContext: DbContext
     {
         public DatabaseContext(DbContextOptions options): base(options)
-        {
-
-        }
-
+        {}        
         public DbSet<Country> Countries { get; set; }
-
         public DbSet<Hotel> Hotels { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Nepal",
+                    ShortName = "NP"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "United States of America",
+                    ShortName = "USA"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "England",
+                    ShortName = "ENG"
+                }
+                );
+            builder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Marriot",
+                    Address = "Kathmandu",
+                    CountryID = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Hilton",
+                    Address = "New York",
+                    CountryID = 2,
+                    Rating = 4
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Novotel",
+                    Address = "London",
+                    CountryID = 3,
+                    Rating = 4.6
+                }
+                );
+        }
     }
 }
---
+++ HotelListing/Migrations/20210317023519_SeedingData.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelListing.Migrations
+{
+    public partial class SeedingData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 1, "Nepal", "NP" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 2, "United States of America", "USA" });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[] { 3, "England", "ENG" });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryID", "Name", "Rating" },
+                values: new object[] { 1, "Kathmandu", 1, "Marriot", 4.5 });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryID", "Name", "Rating" },
+                values: new object[] { 2, "New York", 2, "Hilton", 4.0 });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryID", "Name", "Rating" },
+                values: new object[] { 3, "London", 3, "Novotel", 4.5999999999999996 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
